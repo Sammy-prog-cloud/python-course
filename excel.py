b38beb533395b8197622c1d30fc62972779f0f02
Sammy-prog-cloud <67852652+Sammy-prog-cloud@users.noreply.github.com>
2020-10-27 15:31:49 +0100

    Add files via upload

---
+++ excel.py
@@ -0,0 +1,27 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+sheet.cell(1, 1)
+# print(cell.value)
+# print(sheet.max_row)
+# print(sheet.max_column)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+values = Reference(sheet,
+                   min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=4,
+                   max_col=4)
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
+
+
+wb.save('transaction2.xlsx')
